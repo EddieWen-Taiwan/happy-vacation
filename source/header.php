45c3f750d0b2d61a6faba10f30d3e1130e3a473d
EddieWen-Taiwan <m20021991y@gmail.com>
2015-12-28 20:16:57 +0800

    Add arrow image in month-btn add rotate it

--- source/header.php
+++ source/header.php
@@ -3,7 +3,11 @@
 	<h2>替代役男與他的快樂假期</h2>
 	<div class="subtitle">你知道你不能連放十天假嗎？</div>
 
-	<div class="month-btn prev"></div>
-	<div class="month-btn next"></div>
+	<div class="month-btn prev">
+		<img src="images/arrow_down.png" />
+	</div>
+	<div class="month-btn next">
+		<img src="images/arrow_down.png" />
+	</div>
 
 </header>
