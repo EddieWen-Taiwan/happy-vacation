133d033b8dd10adc96d9f5e28e8ee15e9c52dec5
EddieWen-Taiwan <m20021991y@gmail.com>
2015-12-28 21:07:29 +0800

    variable eventArray

--- js/main.js
+++ js/main.js
@@ -8,28 +8,30 @@ $(document).ready( function(){
 
 	// Get this from user
 	var finalDay = new Date('2016/05/27');
+	var eventArray = [
+		{
+			title: "退伍日",
+			start: '2016/05/27',
+			allDay: true,
+			className: 'retireDate'
+		}
+	];
 
 	// Initialize calendar
-	$('#calendar').fullCalendar({
-		events: [
-			{
-				title: "退伍日",
-				start: '2016/05/27',
-				allDay: true,
-				className: 'retireDate'
-			}
-		]
+	var $calendar = $('#calendar');
+	$calendar.fullCalendar({
+		events: eventArray
 	});
 
 	// Trigger calendar to next/prev month
 	$('.month-btn.prev').on( 'click', function(){
-		$('#calendar').fullCalendar('prev');
+		$calendar.fullCalendar('prev');
 	});
 	$('.month-btn.next').on( 'click', function(){
-		$('#calendar').fullCalendar('next');
+		$calendar.fullCalendar('next');
 	});
 
-	$('#calendar').fullCalendar('gotoDate', finalDay);
+	$calendar.fullCalendar('gotoDate', finalDay);
 
 });
 
