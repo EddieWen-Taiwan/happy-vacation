3d8a89936d91f131ab9d8a1cdfe7753b6df1b83a
EddieWen-Taiwan <m20021991y@gmail.com>
2015-12-28 20:19:37 +0800

    Add function to buttons

--- js/main.js
+++ js/main.js
@@ -6,6 +6,15 @@ $(document).ready( function(){
 	windowW = $(window).width();
 	windowH = $(window).height();
 
+	// Initialize calendar
 	$('#calendar').fullCalendar({});
+
+	// Trigger calendar to next/prev month
+	$('.month-btn.prev').on( 'click', function(){
+		$('#calendar').fullCalendar('prev');
+	});
+	$('.month-btn.next').on( 'click', function(){
+		$('#calendar').fullCalendar('next');
+	});
 
 });
