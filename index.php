9f1c221becc46eb523ffab4f0d6d8d49ba7a2ddf
EddieWen-Taiwan <m20021991y@gmail.com>
2015-12-28 22:45:21 +0800

    Add input content

--- index.php
+++ index.php
@@ -11,6 +11,11 @@
 			<div class="title">
 				<div class="welcome">歡迎來到快樂假期</div>
 			</div>
+			<div class="content">
+				<div class="question"><img src="images/retireDate.png" />入伍日期：</div>
+				<div class="answer">
+				</div>
+			</div>
 		</div>
 	</div>
 
