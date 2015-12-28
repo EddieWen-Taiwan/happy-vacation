27c50bca0971772bc42519aeb2c97a706eceb77a
EddieWen-Taiwan <m20021991y@gmail.com>
2015-12-28 22:32:53 +0800

    Add a card to ask user his retireDate

--- index.php
+++ index.php
@@ -6,6 +6,14 @@
 
 	<div id="calendar"></div>
 
+	<div class="overlay v-mid">
+		<div class="card">
+			<div class="title">
+				<div class="welcome">歡迎來到快樂假期</div>
+			</div>
+		</div>
+	</div>
+
 </body>
 
 </html>
