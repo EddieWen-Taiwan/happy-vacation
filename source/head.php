42e7eba41fd3c362920225a169ad232fc1e569f0
EddieWen-Taiwan <m20021991y@gmail.com>
2015-12-29 10:19:31 +0800

    Add favicon and website-image

--- source/head.php
+++ source/head.php
@@ -7,22 +7,22 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-	<meta name="description" content="網站描述" />
-	<meta name="author" content="作者資訊" />
+	<meta name="description" content="＃你媽知道你在這當爽兵嗎？" />
+	<meta name="author" content="Eddie Wen | 溫奕書" />
 	<meta name="URL" content="網站URL" />
-	<meta name="image" content="網站縮圖" />
+	<meta name="image" content="images/website-image.png" />
 
 	<!-- Open Graph -->
-	<meta property="og:site_name" content="網站標題" />
-	<meta property="og:title" content="小標題(吧" />
+	<meta property="og:site_name" content="替代役男與他的快樂假期" />
+	<meta property="og:title" content="移民署的役男們" />
 	<meta property="og:type" content="website" />
 	<meta property="og:url" content="網站URL"  />
-	<meta property="og:image" content="網站縮圖" />
-	<meta property="og:description" content="網站描述" />
+	<meta property="og:image" content="images/website-image.png" />
+	<meta property="og:description" content="＃你媽知道你在這當爽兵嗎？" />
 
-	<title>休退計畫</title>
-	<link rel="Shortcut Icon" type="image/x-icon" href="" />
-	<link rel="apple-touch-icon" href="" />
+	<title>替代役男與他的快樂假期</title>
+	<link rel="Shortcut Icon" type="image/x-icon" href="images/favicon.png" />
+	<link rel="apple-touch-icon" href="images/favicon.png" />
 
 	<!-- google font -->
 	<link href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800" rel="stylesheet" type="text/css">
