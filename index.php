ce65384bcdcae7940087aba5712ee4228bc855f6
EddieWen-Taiwan <m20021991y@gmail.com>
2015-12-29 22:25:26 +0800

    Two class show & fadeOut to make the effect of wave

--- index.php
+++ index.php
@@ -17,21 +17,21 @@
 				<div class="radio-btns">
 					<div class="options option1 valuable">
 						<div class="radio">
-							<!-- <div class="wave"></div> -->
+							<div class="wave"></div>
 							<div class="real-circle"></div>
 						</div>
 						<span class="text">整日</span>
 					</div>
 					<div class="options option2">
 						<div class="radio">
-							<!-- <div class="wave"></div> -->
+							<div class="wave"></div>
 							<div class="real-circle"></div>
 						</div>
 						<span class="text">上午</span>
 					</div>
 					<div class="options option3">
 						<div class="radio">
-							<!-- <div class="wave"></div> -->
+							<div class="wave"></div>
 							<div class="real-circle"></div>
 						</div>
 						<span class="text">下午</span>
