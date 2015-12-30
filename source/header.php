36d3ac4c7ffa42d855d91a20c83a3b217717db1e
EddieWen-Taiwan <m20021991y@gmail.com>
2015-12-30 09:24:48 +0800

    Add setting-btm in header

--- source/header.php
+++ source/header.php
@@ -3,11 +3,14 @@
 	<h2>替代役男與他的快樂假期</h2>
 	<div class="subtitle">＃你媽知道你在這當爽兵嗎？</div>
 
-	<div class="month-btn prev">
+	<div class="circle-btn month-btn prev">
 		<img src="images/arrow_down.png" />
 	</div>
-	<div class="month-btn next">
+	<div class="circle-btn month-btn next">
 		<img src="images/arrow_down.png" />
+	</div>
+	<div class="circle-btn setting-btn">
+		<img src="images/setting.png" />
 	</div>
 
 </header>
