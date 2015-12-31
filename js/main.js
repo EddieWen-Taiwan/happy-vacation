1939e0d9bfdcf10514e206bbbdde0c968799e519
EddieWen-Taiwan <m20021991y@gmail.com>
2015-12-31 09:24:43 +0800

    Version 1.1

--- js/main.js
+++ js/main.js
@@ -1,8 +1,75 @@
+var eventArray;
+var movedEvent;
+
 $(document).ready( function(){
 
 	// Initialize FullCalendar
 	var $calendar = $('#calendar');
-	$calendar.fullCalendar();
+	$calendar.fullCalendar({
+		eventClick: function( thisEvent, jsEvent, view ) {
+			movedEvent = thisEvent;
+		},
+		eventAfterAllRender: function(event) {
+			$.each( $('.tenDays'), function(){
+				$(this).children('.fc-content').append('<div class="arrow minus" data-move="minus"></div><div class="arrow plus" data-move="plus"></div><div class="event-background"></div>');
+			});
+		}
+	});
+
+	// Arrows in Calendar
+	$calendar.on( 'click', '.arrow', function(){
+		var eventOrdering = parseInt( movedEvent.className[1].substring(6,7) )+1;
+
+		var move = $(this).attr('data-move');
+
+		// Check minus or plus too much
+		var actionPermission = "OK";
+		var preDate = eventArray[eventOrdering].start;
+		if( move == "minus" ) {
+			// Prevent more then ten days
+			// Should not be before this date
+			var limitDate = moment(eventArray[eventOrdering-1].start).add( -10, 'days' );
+
+			preDate = moment(preDate).add( preDate.day() == 1 ? -3 : -1, 'days' );
+			if( preDate.isBefore(limitDate) ) {
+				actionPermission = "NOT ALLOWED";
+				showDialog('left');
+			}
+		} else {
+			// Prevent conflicts
+			preDate = moment(preDate).add( 1, 'days' );
+			preDate.fixWeekend();
+			if( preDate.isSame(eventArray[eventOrdering-1].start) ) {
+				actionPermission = "NOT ALLOWED";
+				showDialog('right');
+			}
+		}
+
+		if( actionPermission == "OK" ) {
+			for( i = eventOrdering; i < 9+1; i++ ) {
+
+				var updatedDate = eventArray[i].start;
+				if( i == eventOrdering ) {
+					// From Mon. to Fri.
+					var dayToMove = 1;
+					if( move == "minus" ) {
+						dayToMove = updatedDate.day() == 1 ? -3 : -1;
+					}
+					// Event be moved
+					updatedDate = moment(updatedDate).add( dayToMove, 'days' );
+				} else {
+					// after that event
+					updatedDate = moment(eventArray[i-1].start).add( -10, 'days' );
+				}
+				updatedDate.fixWeekend();
+
+				eventArray[i].start = updatedDate;
+
+			}
+			$calendar.fullCalendar( 'removeEvents' );
+			$calendar.fullCalendar( 'addEventSource', eventArray );
+		}
+	});
 
 	// Initialize Pikaday
 	var picker = new Pikaday({
@@ -18,7 +85,7 @@ $(document).ready( function(){
 
 			// Get this from user
 			var finalDay = moment( $('#datepicker').val() );
-			var eventArray = [
+			eventArray = [
 				{
 					title: "退伍日",
 					start: finalDay,
@@ -31,23 +98,18 @@ $(document).ready( function(){
 			for( i = 0; i < 9; i++ ) {
 
 				lastEvent = moment(lastEvent).add( -10, 'days' );
-				// lastEvent.day()
-				// 0 -> Sun. // 6 -> Sat.
-				if ( lastEvent.day() == 0 ) {
-					lastEvent = moment(lastEvent).add( 1, 'days' );
-				} else if ( lastEvent.day() == 6 ) {
-					lastEvent = moment(lastEvent).add( 2, 'days' );
-				}
+				lastEvent.fixWeekend();
 
 				var newEvent = {
 					title: "*該上勤了吧",
 					start: lastEvent,
 					allDay: true,
-					className: 'tenDays'
+					className: 'tenDays event-'+i
 				};
 				eventArray.push(newEvent);
 
 			};
+			$calendar.fullCalendar( 'removeEvents' );
 			$calendar.fullCalendar( 'addEventSource', eventArray );
 
 			$calendar.fullCalendar( 'gotoDate', finalDay );
@@ -59,11 +121,34 @@ $(document).ready( function(){
 	});
 
 	// Trigger calendar to next/prev month
-	$('.month-btn.prev').on( 'click', function(){
-		$calendar.fullCalendar('prev');
+	$('.month-btn').on( 'click', function(){
+		$calendar.fullCalendar( $(this).hasClass('prev') ? 'prev' : 'next' );
 	});
-	$('.month-btn.next').on( 'click', function(){
-		$calendar.fullCalendar('next');
+
+	$('.setting-btn').on( 'click', function(){
+		$('.overlay').fadeIn(500);
 	});
 
 });
+
+function showDialog( action ) {
+	$('.alert').addClass('show ' + action);
+	$('.dialog').addClass('bounceIn');
+
+	$('.dialog .fine').on( 'click', function(){
+		$('.alert').removeClass('show left right');
+		$('.dialog').removeClass('bounceIn');
+		$(this).off('click');
+	});
+}
+
+moment.fn.fixWeekend = function() {
+	// lastEvent.day()
+	// 0 -> Sun. // 6 -> Sat.
+	if( this.day() == 0 ) {
+		this.add( 1, 'days' );
+	} else if ( this.day() == 6 ) {
+		this.add( 2, 'days' );
+	}
+}
+
