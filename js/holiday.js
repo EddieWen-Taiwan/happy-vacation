0ce42d2fe5041af2fba74c136cdfd025cd0e6e66
EddieWen-Taiwan <m20021991y@gmail.com>
2016-02-04 20:34:54 +0800

    Isolate holiday

---
+++ js/holiday.js
@@ -0,0 +1,67 @@
+var national_holiday = [
+	{
+		title: "新年",
+		start: moment('2016-02-08'),
+		className: "national"
+	},
+	{
+		title: "新年",
+		start: moment('2016-02-09'),
+		className: "national"
+	},
+	{
+		title: "新年",
+		start: moment('2016-02-10'),
+		className: "national"
+	},
+	{
+		title: "新年",
+		start: moment('2016-02-11'),
+		className: "national"
+	},
+	{
+		title: "新年",
+		start: moment('2016-02-12'),
+		className: "national"
+	},
+	{
+		title: "228補假",
+		start: moment('2016-02-29'),
+		className: "national"
+	},
+	{
+		title: "清明節",
+		start: moment('2016-04-04'),
+		className: "national"
+	},
+	{
+		title: "兒童節補假",
+		start: moment('2016-04-05'),
+		className: "national"
+	},
+	{
+		title: "端午節",
+		start: moment('2016-06-09'),
+		className: "national"
+	},
+	{
+		title: "彈性放假",
+		start: moment('2016-06-10'),
+		className: "national"
+	},
+	{
+		title: "中秋節",
+		start: moment('2016-09-15'),
+		className: "national"
+	},
+	{
+		title: "彈性放假",
+		start: moment('2016-09-16'),
+		className: "national"
+	},
+	{
+		title: "雙十節",
+		start: moment('2016-10-10'),
+		className: "national"
+	}
+];
