3955b3d1ccfafce6d08e479bb08afc207f089c17
EddieWen-Taiwan <m20021991y@gmail.com>
2016-02-15 21:32:10 +0800

    Copy constant

--- js/holiday.js
+++ js/holiday.js
@@ -1,150 +1,150 @@
-var national_holiday = [
+const national_holiday = [
 	{
-		title: "新年",
+		title: '新年',
 		start: moment('2016-02-08'),
-		className: "national"
+		className: 'national',
 	},
 	{
-		title: "新年",
+		title: '新年',
 		start: moment('2016-02-09'),
-		className: "national"
+		className: 'national',
 	},
 	{
-		title: "新年",
+		title: '新年',
 		start: moment('2016-02-10'),
-		className: "national"
+		className: 'national',
 	},
 	{
-		title: "新年",
+		title: '新年',
 		start: moment('2016-02-11'),
-		className: "national"
+		className: 'national',
 	},
 	{
-		title: "新年",
+		title: '新年',
 		start: moment('2016-02-12'),
-		className: "national"
+		className: 'national',
 	},
 	{
-		title: "二二八補假",
+		title: '二二八補假',
 		start: moment('2016-02-29'),
-		className: "national"
+		className: 'national',
 	},
 	{
-		title: "清明節",
+		title: '清明節',
 		start: moment('2016-04-04'),
-		className: "national"
+		className: 'national',
 	},
 	{
-		title: "兒童節補假",
+		title: '兒童節補假',
 		start: moment('2016-04-05'),
-		className: "national"
+		className: 'national',
 	},
 	{
-		title: "端午節",
+		title: '端午節',
 		start: moment('2016-06-09'),
-		className: "national"
+		className: 'national',
 	},
 	{
-		title: "彈性放假",
+		title: '彈性放假',
 		start: moment('2016-06-10'),
-		className: "national"
+		className: 'national',
 	},
 	{
-		title: "中秋節",
+		title: '中秋節',
 		start: moment('2016-09-15'),
-		className: "national"
+		className: 'national',
 	},
 	{
-		title: "彈性放假",
+		title: '彈性放假',
 		start: moment('2016-09-16'),
-		className: "national"
+		className: 'national',
 	},
 	{
-		title: "雙十節",
+		title: '雙十節',
 		start: moment('2016-10-10'),
-		className: "national"
+		className: 'national',
 	},
 	{
-		title: "元旦補假",
+		title: '元旦補假',
 		start: moment('2017-01-02'),
-		className: "national"
+		className: 'national',
 	},
 	{
-		title: "新年",
+		title: '新年',
 		start: moment('2017-01-27'),
-		className: "national"
+		className: 'national',
 	},
 	{
-		title: "新年",
+		title: '新年',
 		start: moment('2017-01-28'),
-		className: "national"
+		className: 'national',
 	},
 	{
-		title: "新年",
+		title: '新年',
 		start: moment('2017-01-29'),
-		className: "national"
+		className: 'national',
 	},
 	{
-		title: "新年",
+		title: '新年',
 		start: moment('2017-01-30'),
-		className: "national"
+		className: 'national',
 	},
 	{
-		title: "新年",
+		title: '新年',
 		start: moment('2017-01-31'),
-		className: "national"
+		className: 'national',
 	},
 	{
-		title: "新年",
+		title: '新年',
 		start: moment('2017-02-01'),
-		className: "national"
+		className: 'national',
 	},
 	{
-		title: "彈性放假",
+		title: '彈性放假',
 		start: moment('2017-02-27'),
-		className: "national"
+		className: 'national',
 	},
 	{
-		title: "二二八",
+		title: '二二八',
 		start: moment('2017-02-28'),
-		className: "national"
+		className: 'national',
 	},
 	{
-		title: "彈性放假",
+		title: '彈性放假',
 		start: moment('2017-04-03'),
-		className: "national"
+		className: 'national',
 	},
 	{
-		title: "兒童節",
+		title: '兒童節',
 		start: moment('2017-04-04'),
-		className: "national"
+		className: 'national',
 	},
 	{
-		title: "清明節",
+		title: '清明節',
 		start: moment('2017-04-05'),
-		className: "national"
+		className: 'national',
 	},
 	{
-		title: "彈性放假",
+		title: '彈性放假',
 		start: moment('2017-05-29'),
-		className: "national"
+		className: 'national',
 	},
 	{
-		title: "端午節",
+		title: '端午節',
 		start: moment('2017-05-30'),
-		className: "national"
+		className: 'national',
 	},
 	{
-		title: "彈性放假",
+		title: '彈性放假',
 		start: moment('2017-10-09'),
-		className: "national"
+		className: 'national',
 	},
 	{
-		title: "雙十節",
+		title: '雙十節',
 		start: moment('2017-10-10'),
-		className: "national"
+		className: 'national',
 	}
 ];
-var weekend_workday = [
+const weekend_workday = [
 	'2016-06-04', '2016-09-10'
 ];
