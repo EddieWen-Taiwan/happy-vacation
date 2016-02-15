0b24c61bc8b605114d4b57dcd6f53701ce13d42c
EddieWen-Taiwan <m20021991y@gmail.com>
2016-02-15 20:21:20 +0800

    Use comma at last item in objects

--- js/main.js
+++ js/main.js
@@ -2,147 +2,147 @@ const national_holiday = [
 	{
 		title: '新年',
 		start: moment('2016-02-08'),
-		className: 'national'
+		className: 'national',
 	},
 	{
 		title: '新年',
 		start: moment('2016-02-09'),
-		className: 'national'
+		className: 'national',
 	},
 	{
 		title: '新年',
 		start: moment('2016-02-10'),
-		className: 'national'
+		className: 'national',
 	},
 	{
 		title: '新年',
 		start: moment('2016-02-11'),
-		className: 'national'
+		className: 'national',
 	},
 	{
 		title: '新年',
 		start: moment('2016-02-12'),
-		className: 'national'
+		className: 'national',
 	},
 	{
 		title: '二二八補假',
 		start: moment('2016-02-29'),
-		className: 'national'
+		className: 'national',
 	},
 	{
 		title: '清明節',
 		start: moment('2016-04-04'),
-		className: 'national'
+		className: 'national',
 	},
 	{
 		title: '兒童節補假',
 		start: moment('2016-04-05'),
-		className: 'national'
+		className: 'national',
 	},
 	{
 		title: '端午節',
 		start: moment('2016-06-09'),
-		className: 'national'
+		className: 'national',
 	},
 	{
 		title: '彈性放假',
 		start: moment('2016-06-10'),
-		className: 'national'
+		className: 'national',
 	},
 	{
 		title: '中秋節',
 		start: moment('2016-09-15'),
-		className: 'national'
+		className: 'national',
 	},
 	{
 		title: '彈性放假',
 		start: moment('2016-09-16'),
-		className: 'national'
+		className: 'national',
 	},
 	{
 		title: '雙十節',
 		start: moment('2016-10-10'),
-		className: 'national'
+		className: 'national',
 	},
 	{
 		title: '元旦補假',
 		start: moment('2017-01-02'),
-		className: 'national'
+		className: 'national',
 	},
 	{
 		title: '新年',
 		start: moment('2017-01-27'),
-		className: 'national'
+		className: 'national',
 	},
 	{
 		title: '新年',
 		start: moment('2017-01-28'),
-		className: 'national'
+		className: 'national',
 	},
 	{
 		title: '新年',
 		start: moment('2017-01-29'),
-		className: 'national'
+		className: 'national',
 	},
 	{
 		title: '新年',
 		start: moment('2017-01-30'),
-		className: 'national'
+		className: 'national',
 	},
 	{
 		title: '新年',
 		start: moment('2017-01-31'),
-		className: 'national'
+		className: 'national',
 	},
 	{
 		title: '新年',
 		start: moment('2017-02-01'),
-		className: 'national'
+		className: 'national',
 	},
 	{
 		title: '彈性放假',
 		start: moment('2017-02-27'),
-		className: 'national'
+		className: 'national',
 	},
 	{
 		title: '二二八',
 		start: moment('2017-02-28'),
-		className: 'national'
+		className: 'national',
 	},
 	{
 		title: '彈性放假',
 		start: moment('2017-04-03'),
-		className: 'national'
+		className: 'national',
 	},
 	{
 		title: '兒童節',
 		start: moment('2017-04-04'),
-		className: 'national'
+		className: 'national',
 	},
 	{
 		title: '清明節',
 		start: moment('2017-04-05'),
-		className: 'national'
+		className: 'national',
 	},
 	{
 		title: '彈性放假',
 		start: moment('2017-05-29'),
-		className: 'national'
+		className: 'national',
 	},
 	{
 		title: '端午節',
 		start: moment('2017-05-30'),
-		className: 'national'
+		className: 'national',
 	},
 	{
 		title: '彈性放假',
 		start: moment('2017-10-09'),
-		className: 'national'
+		className: 'national',
 	},
 	{
 		title: '雙十節',
 		start: moment('2017-10-10'),
-		className: 'national'
+		className: 'national',
 	}
 ];
 const weekend_workday = [
@@ -182,7 +182,7 @@ $(document).ready( function(){
 	// Initialize Pikaday
 	let picker = new Pikaday({
 		field: $('#datepicker')[0],
-		format: 'YYYY-MM-DD'
+		format: 'YYYY-MM-DD',
 	});
 
 	$('.options').on( 'click', function(){
@@ -207,7 +207,7 @@ $(document).ready( function(){
 				{
 					title: '退伍日',
 					start: finalDay,
-					className: 'retireDate'
+					className: 'retireDate',
 				}
 			];
 
@@ -221,7 +221,7 @@ $(document).ready( function(){
 				let newEvent = {
 					title: '*該上勤了吧',
 					start: lastEvent,
-					className: `tenDays event-${i}`
+					className: `tenDays event-${i}`,
 				};
 				eventArray.push(newEvent);
 
