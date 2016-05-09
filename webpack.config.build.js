a68b292db716c70794e7e7db673c176548ca7b1f
EddieWen-Taiwan <m20021991y@gmail.com>
2016-05-09 16:24:53 +0800

    webpack.config for build

---
+++ webpack.config.build.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+	entry: path.resolve(__dirname, 'src', 'app.js'),
+	output: {
+		path: path.resolve(__dirname, 'build'),
+		publicPath: 'http://localhost/happy-vacation/master/build/',
+		filename: 'bundle.js'
+	},
+	module: {
+		loaders: [
+			{ test: /\.js$/, include: /src/, loader: 'babel-loader' },
+			{ test: /\.css$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader') },
+			{ test: /\.(png|jpg)$/, loader: 'file-loader?name=img/[hash:7].[ext]' }
+		],
+		noParse: [/moment/]
+	},
+	resolve: {
+		root: path.resolve(__dirname, 'src'),
+		extensions: ['', '.js'],
+		alias: {
+			moment: 'moment/min/moment.min.js'
+		}
+	},
+	plugins: [
+		new ExtractTextPlugin('all.min.css')
+	]
+}
